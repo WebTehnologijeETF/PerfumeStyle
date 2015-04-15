bd96e8e13200fdab693e0057f04843703fb35a42
hhadzic <hhadzic1@etf.unsa.ba>
2015-04-15 17:31:56 +0200

    Padajuci meni
    Ubacen samo padajuci meni na our friends, ali nije uredjen css , c

---
+++ skripta.js
@@ -0,0 +1,13 @@
+function pokaziMeni(){
+
+	var submeni=podMenu.children[0];
+    var submeni1=podMenu.children[1]; 
+	submeni.style.display="block";
+	submeni1.style.display="block";
+	podMenu.style.display="block";
+    
+ }
+ function sakrijMeni(){
+	 
+  podMenu.style.display="none";
+ }
