4a8394dae425c948290223c0bdd3ad5ad3d95bd5
hhadzic <hhadzic1@etf.unsa.ba>
2015-04-15 22:04:00 +0200

    Validacija

--- skripta.js
+++ skripta.js
@@ -11,3 +11,102 @@ function pokaziMeni(){
 	 
   podMenu.style.display="none";
  }
+
+ function validirajfname(fname)
+{
+    if(fname=="" || fname==null)
+        return false;
+    
+    return true;
+}
+function validirajlname(lname)
+{
+    if(lname=="" || lname==null)
+        return false;
+    
+    return true;
+}
+
+function approveName(){
+	var fname=document.getElementById("fname").value;
+	if(validirajfname(fname)) {
+		document.getElementById("errorfname").innerHTML="";
+		document.getElementById("upozorenje").style.display = "none";}    
+
+	else {
+	document.getElementById("errorfname").innerHTML="Please enter a valid name."; 
+		document.getElementById("upozorenje").style.display = "inline";
+	}	
+}
+
+function approveLName(){
+	var lname=document.getElementById("lname").value;
+	if(validirajlname(lname)) {
+		document.getElementById("errorprezime").innerHTML="";
+		document.getElementById("prezimeslika").style.display = "none";}    
+
+	else {
+	document.getElementById("errorprezime").innerHTML="Please enter a valid last name."; 
+	document.getElementById("prezimeslika").style.display = "inline";
+	}	
+}
+
+
+function validateEmail(mejl) {
+ var re = /\S+@\S+\.\S+/;
+ return re.test(mejl);
+ }
+ 
+ function approveMail(){
+	var email=document.getElementById("mejl").value;
+	if(validateEmail(email))
+	{ document.getElementById("errormail").innerHTML=""; 
+		document.getElementById("mailslika").style.display = "none";}    
+
+	
+	else 
+	{
+		document.getElementById("errormail").innerHTML="Please enter a valid mail.";
+				document.getElementById("mailslika").style.display = "inline";
+
+		}
+}
+
+
+function approveTexarea(){
+	var email = document.getElementById("mejl").value;
+	var tekst=document.getElementById("textPoruke");
+	var ime=document.getElementById("fname").value;
+	var prezime=document.getElementById("lname").value;
+	if(validirajEmail(email) && validirajIme(ime)&& validirajlname(prezime) ){
+		document.getElementById("errorTexarea").innerHTML="";
+       tekst.disabled=false;
+	}
+
+else{
+	tekst.disabled=true;
+	document.getElementById("errorTexarea").innerHTML="Input all data";
+}
+}
+
+function resetall(){
+	
+	document.getElementById("errorfname").innerHTML = "";
+	document.getElementById("errormail").innerHTML="";
+		document.getElementById("errorprezime").innerHTML = "";
+
+	document.getElementById("errorTexarea").innerHTML="";
+	
+	document.getElementById("upozorenje").style.display = "none";
+			document.getElementById("prezimeslika").style.display = "none";
+					document.getElementById("mailslika").style.display = "none";
+	}    
+
+/*
+ function validiraj(){
+	var ime=document.getElementById("fname").value;
+	if(!validirajfname(ime)){
+			return false;
+		}
+			 
+} */
