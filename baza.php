f99b70e33969740607dc840516000cdc283cd2bc
hhadzic <hhadzic1@etf.unsa.ba>
2015-05-29 01:32:22 +0200

    5

--- baza.php
+++ baza.php
@@ -2,11 +2,11 @@
 
 function pdo()
 {
-	$server         = '127.7.84.2';
+	$server         = 'localhost';
 	$port           = 3306;
-	$naziv          = 'parfumestyle';
-	$korisnicko_ime = 'admin8XrcEWZ';
-	$lozinka        = 'WtfYICDsZYYP';
+	$naziv          = 'perfumestyle';
+	$korisnicko_ime = 'root';
+	$lozinka        = '';
 	$konekcija = "mysql:host={$server};port={$port};dbname={$naziv}";
 
 	return new PDO($konekcija, $korisnicko_ime, $lozinka);
